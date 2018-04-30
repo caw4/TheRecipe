454abdf880b2245e3fdfd4df1cd45c0c5080886f
Cassidy <caws4@illinois.edu>
2018-04-30 18:49:33 -0500

    kinda works

--- app/src/main/java/edu/illinois/cs/cs125/therecipe/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/therecipe/MainActivity.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -24,10 +27,21 @@ import org.json.JSONObject;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 
 public class MainActivity extends Activity {
+    private static final String TAG = "USETHISTheRecipe:Main";
+    private static RequestQueue requestQueue;
 
-    Button newButton,saveButton,openButton;
+    Button newButton, saveButton, openButton;
     EditText text;
 
     @Override
@@ -35,11 +49,58 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        newButton=(Button)findViewById(R.id.newButton);
-        saveButton=(Button)findViewById(R.id.saveButton);
-        openButton=(Button)findViewById(R.id.openButton);
-        text=(EditText)findViewById(R.id.text);
+        newButton = (Button) findViewById(R.id.newButton);
+        saveButton = (Button) findViewById(R.id.saveButton);
+        openButton = (Button) findViewById(R.id.openButton);
+        text = (EditText) findViewById(R.id.text);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        final Button randomButton = findViewById(R.id.randomButton);
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Start API button clicked");
+                text.setText(getName("https://www.themealdb.com/api/json/v1/1/random.php"));
+            }
+        });
     }
+    public static String getName(final String json) {
+        JsonParser parser = new JsonParser();
+        JsonObject allrecipe = parser.parse(json).getAsJsonObject();
+        JsonObject meals = allrecipe.get("strMeal").getAsJsonObject();
+        return meals.get("strMeal").getAsString();
+    }
+        void startAPIcall () {
+            try {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.GET,
+                        "https://www.themealdb.com/api/json/v1/1/random.php",
+                        (String) null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(final JSONObject response) {
+                                if (response != null) {
+                                    final TextView text = findViewById(R.id.text);
+                                    if (text == null) {
+                                        Log.d(TAG, "Null pointer.");
+                                    } else {
+                                        text.setText(response.toString());
+                                    }
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(final VolleyError error) {
+                        Log.w(TAG, error.toString());
+                    }
+                });
+                requestQueue.add(jsonObjectRequest);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
 
     public class JsonReader {
 
@@ -73,26 +134,26 @@ public class MainActivity extends Activity {
     }
 
     public void buttonAction(View v) {
-        final EditText fileName=new EditText(this);
-        AlertDialog.Builder ad=new AlertDialog.Builder(this);
+        final EditText fileName = new EditText(this);
+        AlertDialog.Builder ad = new AlertDialog.Builder(this);
         ad.setView(fileName);
 
         if (v.getId() == R.id.saveButton) {
             ad.setMessage("Save File");
 
-            ad.setPositiveButton("Save",new DialogInterface.OnClickListener() {
+            ad.setPositiveButton("Save", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     try {
-                        FileOutputStream fout=openFileOutput(fileName.getText().toString()+".txt", Context.MODE_PRIVATE);
+                        FileOutputStream fout = openFileOutput(fileName.getText().toString() + ".txt", Context.MODE_PRIVATE);
                         fout.write(text.getText().toString().getBytes());
                     } catch (Exception e) {
-                        Toast.makeText(getApplicationContext(), "Error Occured: "+e,Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Error Occured: " + e, Toast.LENGTH_LONG).show();
                     }
                 }
             });
 
-            ad.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+            ad.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     dialog.cancel();
@@ -103,10 +164,10 @@ public class MainActivity extends Activity {
 
         }
 
-        if(v.getId()==R.id.openButton) {
+        if (v.getId() == R.id.openButton) {
             ad.setMessage("Open File");
 
-            ad.setPositiveButton("Open",new DialogInterface.OnClickListener() {
+            ad.setPositiveButton("Open", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
 
@@ -114,19 +175,18 @@ public class MainActivity extends Activity {
                     text.setText("");
 
                     try {
-                        FileInputStream fin = openFileInput(fileName.getText().toString()+".txt");
+                        FileInputStream fin = openFileInput(fileName.getText().toString() + ".txt");
 
-                        while ((c = fin.read()) != -1)
-                        {
+                        while ((c = fin.read()) != -1) {
                             text.setText((text.getText().toString() + Character.toString((char) c)));
                         }
-                    }catch (Exception e) {
-                        Toast.makeText(getApplicationContext(), "Error Occured: "+e,Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error Occured: " + e, Toast.LENGTH_LONG).show();
                     }
                 }
             });
 
-            ad.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+            ad.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     dialog.cancel();
@@ -135,13 +195,8 @@ public class MainActivity extends Activity {
 
             ad.show();
         }
-
-        if(v.getId()==R.id.newButton) {
+        if (v.getId() == R.id.newButton) {
             text.setText("");
         }
-        if(v.getId()==R.id.randomButton) {
-            JsonReader;
-        }
-
     }
 }
